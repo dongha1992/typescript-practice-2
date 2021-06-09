7410401432342e2913223090753877934fe0ed95
김동하 <kamdunga2002@gmail.com>
2021-06-09 16:02:03 +0900

    test: test git config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+//test
 const config = {
 	mode: 'development',
 	entry: './src/index.tsx',
